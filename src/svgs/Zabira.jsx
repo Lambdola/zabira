757bcb725fd69de2fe9c6cf269998c0175f99660
Olalekan Oladimeji <elijahdimeji549@gmail.com>
2026-01-09 05:43:57 +0100

    implement auth screens

--- src/svgs/Zabira.jsx
+++ src/svgs/Zabira.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function SVGZabira() {
+function SVGZabira({width = "115", height = "24"}) {
   return (
     <svg
-      width="115"
-      height="24"
+      width={width}
+      height={height}
       viewBox="0 0 115 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
